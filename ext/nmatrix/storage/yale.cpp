f352edee7f6a589db7128151b880f9f7839bcdd5
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2013-06-14 23:27:50 +0530

    amended a mistake in the correction

--- ext/nmatrix/storage/yale.cpp
+++ ext/nmatrix/storage/yale.cpp
@@ -940,11 +940,11 @@ static char vector_insert_resize(YALE_STORAGE* s, size_t current_size, size_t po
   
   // Copy all values subsequent to the insertion site to the new IJA and new A, leaving room (size n) for insertion.
   if (struct_only) {
-    for (size_t i = pos; i < current_size; ++i) {
+    for (size_t i = pos; i < current_size + n - 1; ++i) {
       new_ija[i+n] = old_ija[i];
     }
   } else {
-    for (size_t i = pos; i < current_size; ++i) {
+    for (size_t i = pos; i < current_size + n - 1; ++i) {
       new_ija[i+n] = old_ija[i];
       new_a[i+n] = old_a[i];
     }
