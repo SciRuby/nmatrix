47893a803295e9d3af4e2818066605e9484a1ead
Sameer Deshmukh <sameer.deshmukh93@gmail.com>
2014-08-20 18:33:22 +0530

    Started with linear interp

---
+++ spec/interpolate/1d_interpolation_spec.rb
@@ -0,0 +1,35 @@
+# = NMatrix
+#
+# A linear algebra library for scientific computation in Ruby.
+# NMatrix is part of SciRuby.
+#
+# NMatrix was originally inspired by and derived from NArray, by
+# Masahiro Tanaka: http://narray.rubyforge.org
+#
+# == Copyright Information
+#
+# SciRuby is Copyright (c) 2010 - 2014, Ruby Science Foundation
+# NMatrix is Copyright (c) 2012 - 2014, John Woods and the Ruby Science Foundation
+#
+# Please see LICENSE.txt for additional copyright notices.
+#
+# == Contributing
+#
+# By contributing source code to SciRuby, you agree to be bound by
+# our Contributor Agreement:
+#
+# * https://github.com/SciRuby/sciruby/wiki/Contributor-Agreement
+#
+# == io_spec.rb
+#
+# Tests for 1D interpolation. Only linear interpolation impelemented as of now.
+
+require 'spec_helper'
+require './lib/nmatrix'
+
+describe NMatrix::Interp1D , :focus => true do
+
+  it "tests if linear interpolation returns correct results" do
+    x = create_vector :dense
+  end
+end
